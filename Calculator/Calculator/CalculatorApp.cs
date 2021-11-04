479f072a63a1f9298b99363948eb64a7e2eac715
Kristian Mateka <kristian.mateka@gmail.com>
2021-11-04 19:03:55 +0100

    init

---
+++ Calculator/Calculator/CalculatorApp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Calculator
+{
+    public class CalculatorApp
+    {
+        public int Sum(int number1, int number2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Substract(int number1, int number2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Multiply(int number1, int number2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Divide(int number1, int number2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Calculator/Tests/CalculatorUnitTests.cs
@@ -0,0 +1,38 @@
+using Calculator;
+using Xunit;
+
+namespace Tests
+{
+    public class CalculatorUnitTests
+    {
+        private readonly CalculatorApp calculator = new CalculatorApp();
+
+        [Fact]
+        public void Calculator_Sum()
+        {
+            var result = calculator.Sum(5, 7);
+            Assert.Equal(12, result);
+        }
+
+        [Fact]
+        public void Calculator_Substract()
+        {
+            var result = calculator.Substract(5, 7);
+            Assert.Equal(-2, result);
+        }
+
+        [Fact]
+        public void Calculator_Multiply()
+        {
+            var result = calculator.Multiply(5, 7);
+            Assert.Equal(35, result);
+        }
+
+        [Fact]
+        public void Calculator_Divide()
+        {
+            var result = calculator.Divide(2, 5);
+            Assert.Equal(0.4, result);
+        }
+    }
+}
